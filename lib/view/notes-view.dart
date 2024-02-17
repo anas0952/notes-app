1449d4bcb23f51d23e6a24d67fc1459faa8b392e
anas <>
2024-02-17 08:40:29 +0300

    add them & create notesview

---
+++ lib/view/notes-view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotesAppView extends StatelessWidget {
+  const NotesAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}

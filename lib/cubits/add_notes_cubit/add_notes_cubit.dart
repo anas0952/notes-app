a78b0d4c396e76657ad7d2c6f5493a2313f05e56
anas <>
2024-02-19 14:31:16 +0300

    add notes & add box

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app_view/constants.dart';
+import 'package:notes_app_view/model/notes-model.dart';
 
 part 'add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+
+  addNote(NotesModel notesModel) async {
+    emit(AddNotesLoading());
+    try {
+      var notesBox = await Hive.openBox<NotesModel>(kHivebox);
+      notesBox.add(notesModel);
+      emit(AddNotesSuccess());
+    } catch (e) {
+      // TODO
+      emit(AddNotesFaliure(errMessage: e.toString()));
+    }
+  }
 }
